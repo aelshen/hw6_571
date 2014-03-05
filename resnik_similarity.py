94d3ca14a9b641c1e457bce0fb01d7153fca66cd
aelshen <ahmad.k.elshenawy@gmail.com>
2014-03-04 17:30:49 -0800

    More or less complete
    Assignment seems more-or-less ready

--- resnik_similarity.py
+++ resnik_similarity.py
@@ -1,15 +1,18 @@
 '''
 #==============================================================================
 resnik_similarity
-/Users/aelshen/Documents/Dropbox/School/CLMS 2013-2014/Winter 2014/Ling 571-Deep Processing Techniques for NLP/hw6_571/resnik_similarity.py
-Created on Feb 28, 2014
+Created on Mar 6, 2014
 @author: aelshen
 #==============================================================================
 '''
 
+from __future__ import print_function
 import os
 import sys
+import math
 from nltk.corpus import wordnet as wn
+from nltk.corpus import wordnet_ic
+from nltk.corpus.reader.wordnet import information_content
 from collections import defaultdict
 #==============================================================================
 #--------------------------------Constants-------------------------------------
@@ -21,10 +24,22 @@ DEBUG = True
 #==============================================================================
 def main():
     context_file = sys.argv[1]
+    ic_file = sys.argv[2]
     
-    context_dict = LoadContextFile(context_file)
     
-    print("Hello, World!")
+    context = LoadContextFile(context_file)
+    brown_ic = wordnet_ic.ic(ic_file)
+    
+    results = Process(context, brown_ic)
+    
+    
+    ######
+    #EXAMPLES
+    ######
+    #dog = wn.synset('dog.n.01')
+    #cat = wn.synset('cat.n.01')
+    #Calculate resnik similarity
+    #dog.res_similarity(cat, brown_ic)
 #==============================================================================    
 #---------------------------------Functions------------------------------------
 #==============================================================================
@@ -40,38 +55,96 @@ def main():
 ##        Returns:            returns
 ##-------------------------------------------------------------------------
 def LoadContextFile(context_file):
-    context_dict = defaultdict(set)
+    context = []
     
     for line in open(context_file, 'r'):
         line = line.strip().split()
         probe_word = line[0]
         word_grouping = line[1].split(",")
         
-        for word in word_grouping:
-            context_dict[probe_word].add(word)
+        context.append((probe_word, word_grouping))
             
-    return context_dict
+    return context
 
-#==============================================================================    
-#----------------------------------Classes-------------------------------------
-#==============================================================================
-##-------------------------------------------------------------------------
-## Class Classname
-##-------------------------------------------------------------------------
-##    Description:        desciription
-##
-##    Arguments:         arguments
-##
-##
-##    Properties:         properties
-##
-##    Calls:                  calls
-##
-##-------------------------------------------------------------------------
-class Classname:
-    def __init__(self):
-        self.x = 0
 
+##-------------------------------------------------------------------------
+## Process()
+##-------------------------------------------------------------------------
+##    Description:        description
+##
+##    Arguments:        arguments
+##
+##    Calls:                calls
+##
+##        Returns:            returns
+##-------------------------------------------------------------------------
+def Process(context, ic):
+    best_senses = []
+    results = []
+    for i in xrange( len(context) ):
+        normalization_factor = 0.0
+        scores = defaultdict(float)
+        probe_word = context[i][0]
+        for context_word in context[i][1]:            
+            resnik = ResnikSimilarity(probe_word, context_word, ic)
+            
+            if resnik:
+                mis,similarity = resnik
+                print( (probe_word, context_word, similarity), end="" )
+
+                normalization_factor += similarity
+                
+                for sense in wn.synsets(probe_word):
+                    if mis in mis.common_hypernyms(sense):
+                        scores[sense] += similarity
+                    
+            else:
+                print( (probe_word, context_word, 'None'), end="" )
+    
+        
+        for s in scores:
+            scores[s] = scores[s] / normalization_factor
+    
+        print(os.linesep + max(scores.iterkeys(), key=lambda x: scores[x]).name )
+    
+    return results
+        
+##-------------------------------------------------------------------------
+## Resnik_Similarity()
+##-------------------------------------------------------------------------
+##    Description:        description
+##
+##    Arguments:        arguments
+##
+##    Calls:                calls
+##
+##        Returns:            returns
+##-------------------------------------------------------------------------
+def ResnikSimilarity(probe_word, context_word, ic):
+    probe_senses = wn.synsets(probe_word)
+    context_senses = wn.synsets(context_word)
+    
+    if not context_senses:
+        return None
+    
+    most_informative_subsumers = set() 
+    
+    for p in probe_senses:
+        for c in context_senses:
+            common_hypernyms = p.common_hypernyms(c)
+            if common_hypernyms:
+                temp =  max(common_hypernyms, key=lambda x: information_content(x, ic) )
+                score = information_content(temp, ic)
+
+                
+                most_informative_subsumers.add( (temp, score) )
+            
+    
+    mis = max(most_informative_subsumers, key=lambda x: x[1])
+    
+    return mis
+
+    
 #==============================================================================    
 #------------------------------------------------------------------------------
 #==============================================================================
